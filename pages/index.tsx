12c408b0c548471153297a2273274b1b82b985d7
AlbionCannan <tommy@aoun.net>
2025-09-11 00:36:07 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main style={{maxWidth: 720, margin: "40px auto", fontFamily: "system-ui"}}>
+    <main style={{ maxWidth: 720, margin: '40px auto', fontFamily: 'system-ui' }}>
       <h1>It works ✅</h1>
       <p>This is a minimal Next.js app deployed on Vercel.</p>
     </main>
